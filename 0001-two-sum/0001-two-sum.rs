c2417f450bff0f9ac03b69105750dcc286e0b76e
ISOTOPE <97857682+Goitseone-Themba@users.noreply.github.com>
2024-09-30 11:39:26 +0200

    Time: 42 ms (11.03%), Space: 2.2 MB (91.8%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut ans:Vec<i32> = Vec::new();
+
+        for i in 0..nums.len()-1 {
+            for j in i+1..nums.len() {
+                if (nums[i] + nums[j]) == target {
+                    ans.push(i.to_string().trim().parse::<i32>().unwrap());
+                    ans.push(j.to_string().trim().parse::<i32>().unwrap());
+                }
+            }
+        }
+
+        ans
+    }
+}
